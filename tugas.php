4cc0ec4d336f491b9dc76d9bd223a785d3d4c0a8
SyahrinNanda <syahrinnanda@gmail.com>
2022-07-21 11:26:45 +0800

    add title and text

---
+++ tugas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Command Line dan Github</title>
+</head>
+<body>
+    <H1>Tugas Command Line dan Github</H1>
+    <H3>Nama : As'syahrin Nanda</H3>
+    <H3>Kelas: JWD2</H3>
+</body>
+</html>
